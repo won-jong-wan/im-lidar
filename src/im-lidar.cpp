08e86695f459f7accbca05e077a20699ef8291bc
Won <jonwon2009@tukorea.ac.kr>
2024-05-22 21:46:10 +0900

    success opencv

--- src/im-lidar.cpp
+++ src/im-lidar.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
+
+#include <sensor_msgs/image_encodings.h>
+
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -9,21 +11,52 @@ static const std::string OPENCV_WINDOW = "Image window";
 
 class ImageConverter
 {
-private:
-ros::Nod
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
 public:
-  ImageConverter(ros::NodeHandle &nh)
+  ImageConverter():it_(nh_)
   {
+    image_sub_ = it_.subscribe("/camera/color/image_rect_color", 1, &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+
+  ~ImageConverter(){
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg){
+    cv_bridge::CvImagePtr cv_ptr;
+
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch(const std::exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60){
+      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+    }
+
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+
+    image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
 
 int main(int argc, char * argv[])
 {
-  ros::init(argc, argv, "/* node_name */");
-  auto /* node_name */ = /* namespace_name::ClassName */();
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
   ros::spin();
-  ros::shutdown();
   return 0;
 }
 
